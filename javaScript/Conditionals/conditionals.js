5e015d90480885689ad66d386bf2e5218896a65d
Pat Palmerston <patpalmerston@gmail.com>
2021-08-04 11:56:18 -0700

    switch

--- javaScript/Conditionals/conditionals.js
+++ javaScript/Conditionals/conditionals.js
@@ -1,9 +1,21 @@
 console.log("before conditional")
 
-const password = prompt("enter pass")
-
-if(password.length >= 6 && password.indexOf(' ') === -1) {
-  console.log('valid')
-} else {
-  console.log('invalid')
+const day = 2;
+switch(day) {
+  case 1:
+    console.log("h1")
+    break;
+  case 2:
+    console.log("h2")
+    break;
+  case 3:
+    console.log("h3")
+    break;
+  case 4:
+    console.log("h4")
+    break;
+  case 5:
+    console.log("h5")
+    break;
+  
 }
